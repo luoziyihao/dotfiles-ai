5b0fe1067330cb225a5ce95b9023eff1e8867c18
xiang.rao <wangyiraoxiang@163.com>
2019-07-02 13:46:50 +0800

    fix

--- func.sh
+++ func.sh
@@ -3,15 +3,19 @@
 # File Name: test.sh
 # Author: raoxiang
 
+#字符串要包起来
+
 function contain_str(){
     param_count=${#}
     if [ ${param_count} != 2 ]; then 
-        echo "parameter count is illegal. need 2 parameter"
+        echo "parameter count is illegal. need 2 parameter. param_count: ${param_count}"
         return 1
     fi
 
     file_name=${1}
     str=${2}
+    echo "file_name:${file_name}"
+    echo "str:${str}"
 
     if [ ! -f "${file_name}" ]; then
         echo -e "\033[34m ${file_name} is not exist \033[0m"
@@ -24,7 +28,7 @@ function contain_str(){
         return 1
     fi
 
-    rs=`grep ${str} ${file_name}|tail -n 1`
+    rs=`grep "${str}" ${file_name}|tail -n 1`
     if [ "${rs}"x != ""x ]; then
         echo "${file_name} contain ${str}"
         return 0
@@ -33,7 +37,3 @@ function contain_str(){
         return 1
     fi
 }
-
-# contain_str "./tmp.md" "test2222"
-
-# echo ${?}
